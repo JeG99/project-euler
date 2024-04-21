c22eef05e84c55569c3232060dcf625c2f8c5f8d
Oliver García <oliver.garcia1310@gmail.com>
2024-04-21 17:29:12 -0600

    solved 2

---
+++ 2 - Even Fibonacci Numbers/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+import Solution ( solution )
+
+main :: IO ()
+main = do
+    print $ solution 4613732
---
+++ 2 - Even Fibonacci Numbers/Test.hs
@@ -0,0 +1,23 @@
+-- Even Fibonacci Numbers
+-- https://projecteuler.net/problem=2
+
+module Test where
+
+-- Upper Bond Limit Fibbonaci Number
+limit = 4_000_000
+
+-- Fibonacci Recursive Function Definition
+-- oneliner: fib :: Int -> Int; fib 1 = 1; fib 2 = 1; fib n = fib (n-1) + fib (n-2)
+fib :: Int -> Int
+fib 1 = 1
+fib 2 = 1
+fib n = fib (n-1) + fib (n-2)
+
+-- Use all above
+solution :: Int 
+solution = sum $ filter even $ takeWhile (< limit) [fib x | x <- [1..]]
+
+-- Print solution
+main :: IO ()
+main = do
+    print solution
